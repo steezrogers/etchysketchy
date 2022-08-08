031d831fdc369796e4d10683036551ed9ffb9ab4
steezrogers <stevelee83@gmail.com>
2022-08-07 21:35:21 -0700

    Add function to change number of squares in grid
    Added button to trigger function. Function squares inputted value
    to create a value * value grid. Also made minor adjustments to
    styling in css

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 const container = document.querySelector('.container');
 
-function grid() {
-    for (let i = 0; i < 256; i++) {
+function grid(x) {
+    for (let i = 0; i < x**2; i++) {
+        container.style.setProperty('--colNum', x);
+        container.style.setProperty('--rowNum', x);
         let square =  document.createElement('div');
         square.classList.add('square');
         container.appendChild(square);
     }
+    sketch();
 }
 
-grid();
+grid(16);
 
-const etch = document.querySelectorAll('.square');
-etch.forEach(square => square.addEventListener('mouseover', function sketch(event) {
-    event.target.style.backgroundColor = 'black';
-}));
+function sketch() {
+    let etch = document.querySelectorAll('.square');
+        etch.forEach(square => square.addEventListener('mouseover', (event) =>
+        event.target.style.backgroundColor = 'black'));
+}
+
+const change = document.getElementById('change');
+change.addEventListener('click', squares);
+
+function squares() {
+    let box = prompt(`Please enter a value from 16 to 100.`);
+        if ((box > 0 && box < 16) || box > 100 || isNaN(box)) {
+            alert(`Please enter a valid number.`);
+            return squares();
+        } else if (box == null) {
+            return;
+        }
+    remove(container);
+    grid(box);
+}
+
+function remove(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
